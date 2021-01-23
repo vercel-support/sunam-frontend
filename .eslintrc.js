91ab2f573028704ada225e0cc82ccc313e659f67
Joowon <joowon.jang@hotmail.com>
2021-01-23 21:55:04 +0900

    lint, prettier settings

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'prettier/react',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+  ],
+};
