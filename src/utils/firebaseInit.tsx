3cfd2cfa9f55a05d0308ce17ce77a22b93055253
Joowon <joowon.jang@hotmail.com>
2021-02-14 19:07:01 +0900

    env test

--- src/utils/firebaseInit.tsx
+++ src/utils/firebaseInit.tsx
@@ -1,5 +1,5 @@
 import firebase from 'firebase/app';
-import firebaseConfig from '/conf/firebaseConfig';
+// import firebaseConfig from '/conf/firebaseConfig';
 import 'firebase/auth';
 import 'firebase/firestore';
 
@@ -9,20 +9,25 @@ import 'firebase/firestore';
 //   firebase.initializeApp(firebaseConfig);
 // }
 if (process.env.NODE_ENV === 'development') {
-  if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-  } else {
-    const config = {
-      apiKey: process.env.REACT_APP_APIKEY,
-      authDomain: process.env.REACT_APP_AUTHDOMAIN,
-      projectId: process.env.REACT_APP_PROJECTID,
-      storageBucket: process.env.STORAGEBUCKET,
-      messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-      appId: process.env,
-    };
-
-    firebase.initializeApp(config);
-  }
+  const config = {
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID,
+  };
+  firebase.initializeApp(config);
+} else {
+  const config = {
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID,
+  };
+  firebase.initializeApp(config);
 }
 
 export const app = firebase;
