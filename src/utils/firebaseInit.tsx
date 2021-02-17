84d0a7675be3b404bbbe6ae0de008e50567f69a9
Joowon <joowon.jang@hotmail.com>
2021-02-18 01:04:05 +0900

    fix keys

--- src/utils/firebaseInit.tsx
+++ src/utils/firebaseInit.tsx
@@ -6,7 +6,7 @@ const config = {
   apiKey: process.env.REACT_APP_APIKEY,
   authDomain: process.env.REACT_APP_AUTHDOMAIN,
   projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
   appId: process.env.REACT_APP_APPID,
 };
