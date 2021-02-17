f6cb7d784ede7b24885eea7d6a9e812b59ec3670
Joowon <joowon.jang@hotmail.com>
2021-02-18 02:13:56 +0900

    firebase config change

--- src/utils/firebaseInit.tsx
+++ src/utils/firebaseInit.tsx
@@ -1,20 +1,9 @@
 import firebase from 'firebase/app';
+import firebaseConfig from '/conf/firebaseConfig';
 import 'firebase/auth';
 import 'firebase/firestore';
 
-const config = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APPID,
-};
-firebase.initializeApp(config);
-
-// if (!firebase.apps.length) {
-//   firebase.initializeApp(firebaseConfig);
-// }
+firebase.initializeApp(firebaseConfig);
 
 export const app = firebase;
 export const db = firebase.firestore();
