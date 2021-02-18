0df8855590c5182b12d028ac01c99017f9090db0
Joowon <joowon.jang@hotmail.com>
2021-02-18 13:49:41 +0900

    add path

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const { merge } = require('webpack-merge');
 const webpack = require('webpack');
 const common = require('./webpack.common.js');
 const Dotenv = require('dotenv-webpack');
+const path = require('path');
 
 module.exports = merge(common, {
   mode: 'production',
