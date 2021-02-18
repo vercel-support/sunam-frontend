3a808c6776fab530f60c4d7ea25017caab615fed
Joowon <joowon.jang@hotmail.com>
2021-02-18 14:47:41 +0900

    remove settings

--- conf/firebaseConfig.tsx
+++ conf/firebaseConfig.tsx
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
-  databaseURL: process.env.REACT_APP_DATABASEURL,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  databaseURL: process.env.DATABASEURL,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
   appId: process.env.REACT_APP_APPID,
 };
 
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,7 @@
 const { merge } = require('webpack-merge');
 const webpack = require('webpack');
 const common = require('./webpack.common.js');
-const Dotenv = require('dotenv-webpack');
-const path = require('path');
 
 module.exports = merge(common, {
   mode: 'production',
-  plugins: [
-    new Dotenv({
-      path: path.resolve(__dirname, './.env'),
-    }),
-  ],
 });
