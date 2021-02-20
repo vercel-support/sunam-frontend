69f177a40961d3ce3c5b5e0ddd80e28f494424b1
Joowon <joowon.jang@hotmail.com>
2021-02-20 17:46:55 +0900

    sentry add

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from '@/Root';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 // import reportWebVitals from './reportWebVitals';
+
+console.log(process.env.VERCEL_ENV);
+Sentry.init({
+  dsn: `${process.env.REACT_APP_SENTRYDSN}`,
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(<Root />, document.getElementById('root'));
 
