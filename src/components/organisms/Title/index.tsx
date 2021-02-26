82a7c9f9ffc428a93da916bb7dad15dfc39f8ecf
William Li <email@williamli.dev>
2021-02-26 12:01:32 +0800

    added debug messages

--- src/components/organisms/Title/index.tsx
+++ src/components/organisms/Title/index.tsx
@@ -6,7 +6,9 @@ const Title = () => {
     <Styled.Container>
       <Styled.Wrapper>
         <Styled.Title>도움이 필요하신가요?</Styled.Title>
-        <Styled.SubTitle>여러분의 이야기를 들려주세요.</Styled.SubTitle>
+        <Styled.SubTitle>
+          {process.env.REACT_APP_PROJECTID} - 여러분의 이야기를 들려주세요.
+        </Styled.SubTitle>
       </Styled.Wrapper>
     </Styled.Container>
   );
